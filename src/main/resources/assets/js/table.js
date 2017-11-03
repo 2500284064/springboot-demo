734d8ca8a0470350f75e7589d2aeef5a72af8270
njuftz <2500284064@qq.com>
2017-11-03 16:45:57 +0800

    table 插件

--- src/main/resources/assets/js/table.js
+++ src/main/resources/assets/js/table.js
@@ -7,20 +7,30 @@
         title: "",
         columns: [{title: "1",name:"a"},{title: "2",name:"b"},{title: "3",name:"c"},{title: "4",name:"d"}],
         async: false,
-        page: 1,
-        rows: 10,
+        page: 2,
+        rows: 5,
+        total: 10,
+        totalPage: 2,
         url: "",
         data: [
             {a: "1a", b:"1b", c:"1c", d:"1d"},
             {a: "2a", b:"2b", c:"2c", d:"2d"},
-            {a: "3a", b:"3b", c:"3c", d:"3d"}
+            {a: "3a", b:"3b", c:"3c", d:"3d"},
+            {a: "4a", b:"4b", c:"4c", d:"4d"},
+            {a: "5a", b:"5b", c:"5c", d:"5d"},
+            {a: "6a", b:"6b", c:"6c", d:"6d"},
+            {a: "7a", b:"7b", c:"7c", d:"7d"},
+            {a: "8a", b:"8b", c:"8c", d:"8d"}
         ],
-        paginator: null
+        paginator: null,
+        pre: "<i class='fa fa-angle-left' aria-hidden='true'></i>pre",
+        next: "<i class='fa fa-angle-right' aria-hidden='true'></i>next",
     };
 
     var methods = {
         init: function (options) {
-            settings = $.extend(options, settings);
+            settings = $.extend(settings, options);
+            console.log(settings);
             if (settings.url != "") {
                 $.ajax({
                     url: settings.url,
@@ -28,10 +38,18 @@
                     type: "get",
                     async: false,
                     success: function (reply) {
-                        if(reply != null) settings.data = reply;
+                        if(reply != null) {
+                            settings.data = reply;
+                        }
                     }
                 })
             }
+
+            settings.total = settings.data.length;
+            settings.totalPage = Math.ceil(settings.total / settings.rows);
+            if(settings.page <= 0) settings.page = 1;
+            if(settings.page > settings.totalPage) settings.page = settings.totalPage;
+
             methods["genereateTable"].apply(this);
         },
         genereateTable: function () {
@@ -47,13 +65,36 @@
 
             headTr.appendTo(thead);
 
-            $.each(settings.data, function (i, value) {
+            var startIndex = (settings.page - 1) * settings.rows;
+            if(startIndex < 0 ) startIndex = 0;
+            var endIndex = startIndex + settings.rows;
+            var data = settings.data;
+            for(var i = startIndex; i < endIndex && i<data.length; i++){
                 var bodyTr = $("<tr></tr>");
                 $.each(settings.columns, function (j, column) {
-                    $("<td>" + value[column.name] + "</td>").appendTo(bodyTr);
+                    $("<td>" + data[i][column.name] + "</td>").appendTo(bodyTr);
                 });
                 bodyTr.appendTo(tbody);
-            });
+            }
+
+            if(settings.paginator != null && $("#" + settings.paginator).length >= 1){
+                var paginator = $("<div class='paginator'></div>");
+                if(settings.page > 1){
+                    $("<span class='page-icon pre'>" + settings.pre + "</span>").appendTo(paginator);
+                }
+
+                var startPage = settings.page - 5 > 0 ? settings.page - 5 : 1;
+                var endPage = settings.totalPage - settings.page > 5 ? settings.page + 5 : settings.totalPage;
+                for(var i = startPage; i<= endPage; i++){
+                    $("<span class='page-icon middle'>" + i + "</span>").appendTo(paginator);
+                }
+
+                if(settings.page < settings.totalPage){
+                    $("<span class='page-icon next'>" + settings.next + "</span>").appendTo(paginator);
+                }
+
+                paginator.appendTo($("#" + settings.paginator));
+            }
 
             thead.appendTo(table);
             tbody.appendTo(table);
